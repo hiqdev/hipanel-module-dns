5cc02c80dfd207c5513b2641f8bc4234e26fd333
Andrii Vasyliev <sol@hiqdev.com>
2016-05-18 14:07:19 +0000

    used composer-config-plugin and asset-packagist.org

---
+++ src/config/hisite.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * HiPanel DNS Module
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-dns
+ * @package   hipanel-module-dns
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'aliases' => [
+        '@dns' => '/dns/',
+    ],
+    'modules' => [
+        'dns' => [
+            'class' => \hipanel\modules\dns\Module::class,
+        ],
+    ],
+    'components' => [
+        'i18n' => [
+            'translations' => [
+                'hipanel/dns' => [
+                    'class' => \yii\i18n\PhpMessageSource::class,
+                    'basePath' => '@hipanel/modules/dns/messages',
+                    'fileMap' => [
+                        'hipanel/dns' => 'dns.php',
+                    ],
+                ],
+            ],
+        ],
+        'menuManager' => [
+            'menus' => [
+                'dns' => \hipanel\modules\dns\SidebarMenu::class,
+            ],
+        ],
+    ],
+];
