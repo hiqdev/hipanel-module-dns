af95c17197bef92b170830357a675706a6e5eede
tofid <andreyklochok@gmail.com>
2016-05-23 17:02:39 +0300

    minor

--- src/views/zone/_search.php
+++ src/views/zone/_search.php
@@ -3,17 +3,24 @@ use hipanel\modules\client\widgets\combo\ClientCombo;
 use hipanel\modules\client\widgets\combo\SellerCombo;
 use hipanel\modules\hosting\widgets\combo\SshAccountCombo;
 use hipanel\modules\server\widgets\combo\ServerCombo;
-
+/**
+ * @var \hipanel\widgets\AdvancedSearch $search
+ */
 ?>
 
-<div class="col-md-6">
+<div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('domain_like') ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('server')->widget(ServerCombo::className(), ['formElementSelector' => '.form-group']) ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('account')->widget(SshAccountCombo::className(), ['formElementSelector' => '.form-group']) ?>
 </div>
-
-<div class="col-md-6">
+<div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('client_id')->widget(ClientCombo::classname(), ['formElementSelector' => '.form-group']) ?>
+</div>
+<div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('seller_id')->widget(SellerCombo::classname(), ['formElementSelector' => '.form-group']) ?>
 </div>
 
