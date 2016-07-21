60d7696e055118f996b8ac3855c06ec246c4450e
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-07-21 15:33:20 +0300

    Removed Client and Seller filters from the AdvancedSearch view for non-support

--- src/views/zone/_search.php
+++ src/views/zone/_search.php
@@ -3,6 +3,7 @@ use hipanel\modules\client\widgets\combo\ClientCombo;
 use hipanel\modules\client\widgets\combo\SellerCombo;
 use hipanel\modules\hosting\widgets\combo\SshAccountCombo;
 use hipanel\modules\server\widgets\combo\ServerCombo;
+
 /**
  * @var \hipanel\widgets\AdvancedSearch $search
  */
@@ -17,10 +18,12 @@ use hipanel\modules\server\widgets\combo\ServerCombo;
 <div class="col-md-4 col-sm-6 col-xs-12">
     <?= $search->field('account')->widget(SshAccountCombo::className(), ['formElementSelector' => '.form-group']) ?>
 </div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('client_id')->widget(ClientCombo::classname(), ['formElementSelector' => '.form-group']) ?>
-</div>
-<div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('seller_id')->widget(SellerCombo::classname(), ['formElementSelector' => '.form-group']) ?>
-</div>
 
+<?php if (Yii::$app->user->can('support')) { ?>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+        <?= $search->field('client_id')->widget(ClientCombo::classname(), ['formElementSelector' => '.form-group']) ?>
+    </div>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+        <?= $search->field('seller_id')->widget(SellerCombo::classname(), ['formElementSelector' => '.form-group']) ?>
+    </div>
+<?php } ?>
