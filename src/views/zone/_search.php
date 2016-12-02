0758f06674e28a03b760838ff318a9252dca0d9f
Andrii Vasyliev <sol@hiqdev.com>
2016-12-02 09:17:48 +0000

    minor

--- src/views/zone/_search.php
+++ src/views/zone/_search.php
@@ -1,4 +1,5 @@
 <?php
+
 use hipanel\modules\client\widgets\combo\ClientCombo;
 use hipanel\modules\client\widgets\combo\SellerCombo;
 use hipanel\modules\hosting\widgets\combo\SshAccountCombo;
@@ -13,17 +14,17 @@ use hipanel\modules\server\widgets\combo\ServerCombo;
     <?= $search->field('domain_like') ?>
 </div>
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('server')->widget(ServerCombo::className(), ['formElementSelector' => '.form-group']) ?>
+    <?= $search->field('server')->widget(ServerCombo::class, ['formElementSelector' => '.form-group']) ?>
 </div>
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('account')->widget(SshAccountCombo::className(), ['formElementSelector' => '.form-group']) ?>
+    <?= $search->field('account')->widget(SshAccountCombo::class, ['formElementSelector' => '.form-group']) ?>
 </div>
 
-<?php if (Yii::$app->user->can('support')) { ?>
+<?php if (Yii::$app->user->can('support')) : ?>
     <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('client_id')->widget(ClientCombo::classname(), ['formElementSelector' => '.form-group']) ?>
+        <?= $search->field('client_id')->widget(ClientCombo::class, ['formElementSelector' => '.form-group']) ?>
     </div>
     <div class="col-md-4 col-sm-6 col-xs-12">
-        <?= $search->field('seller_id')->widget(SellerCombo::classname(), ['formElementSelector' => '.form-group']) ?>
+        <?= $search->field('seller_id')->widget(SellerCombo::class, ['formElementSelector' => '.form-group']) ?>
     </div>
-<?php } ?>
+<?php endif ?>
