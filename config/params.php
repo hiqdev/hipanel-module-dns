f7ba4d3996366fc53c4352d4ba4bf4adca6dfd4c
Yurii Myronchuk <bladeroot@gmail.com>
2021-06-11 11:06:31 +0300

    show warning if domain has not any served nss (#20)
    * show warning if domain has not any served nss
    
    * rename ns => nss
    
    * Update src/controllers/ZoneController.php
    
    Co-authored-by: Andrii Vasyliev <sol@hiqdev.com>
    
    * minor
    
    Co-authored-by: Dmytro Naumenko <d.naumenko.a@gmail.com>
    
    Co-authored-by: Dmytro Naumenko <d.naumenko.a@gmail.com>
    Co-authored-by: Andrii Vasyliev <sol@hiqdev.com>

---
+++ config/params.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * DNS module for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-dns
+ * @package   hipanel-module-dns
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2021, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'module.dns.default.nss' => [],
+];
